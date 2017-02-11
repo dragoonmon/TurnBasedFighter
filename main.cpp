32bfc2d78c1491ef2e7534a0844a2d5ec6d1a3cb
dragoonmon <jandug3@uni.coventry.ac.uk>
2017-02-11 15:17:50 +0000

    Add files via upload
    Ascii art for both players

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "  _._" << setw(70) << "  _._" << endl;
+    cout << ".'--.`." << setw(70) << ".'--.`." << endl;
+    cout << "|  .' |" << setw(70) << "|  .' |" << endl;
+    cout << " `--`'" << setw(70) << " `--`'" << endl;
+
+
+
+    return 0;
+}
